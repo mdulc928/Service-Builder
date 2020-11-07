81d80a072a8db8fd011db7c5499fabfec865bd13
mdulc928 <mdulc928@students.bju.edu>
2020-11-07 16:28:09 -0500

    Updated to import request as well

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 #   pip install mysql-connector-python
 
 
-from flask import Flask
+from flask import Flask, request
 from datetime import datetime
 import time
 from mysql.connector import connect
@@ -11,17 +11,21 @@ import dbconfig
 
 app = Flask(__name__)
 
+con = connect(user=dbconfig.DB_USER, password=dbconfig.DB_PASS, database='univdb', host=dbconfig.DB_HOST) 
+cursor = con.cursor()
+
+error_msg = ["Another service is being held at the same time."]
+
 @app.route('/')
 def hello():
+    global cursor
     qty = 0
     selectedCourseno = None
-    if 'courseNo' in bottle.request.params:
-        selectedCourseno = bottle.request.params['courseNo']
-
+    if 'courseNo' in request.args:
+        selectedCourseno = request.args["blah"]
     # We don't close the following explicitly because they are automatically closed
     # when the variables go out of scope when hello() returns
-    con = connect(user=dbconfig.DB_USER, password=dbconfig.DB_PASS, database='univdb', host=dbconfig.DB_HOST) 
-    cursor = con.cursor() 
+     
 
     if selectedCourseno != None and selectedCourseno != "":
         cursor.execute("""
@@ -72,6 +76,15 @@ HTML_DOC = """<html><body>
 
 @app.route("/create")
 def create():
+    global cursor
+    
+    #do checks
+    svc_id, svc_datetime, theme = (0, 0, 0)
+
+
+    #create service
+    cursor.callproc('create_service', (svc_id, svc_datetime, theme))
+    #create update fills role
     
 # Launch the BottlePy dev server
 if __name__ == "__main__":
