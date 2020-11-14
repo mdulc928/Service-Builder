0c3bff01c5b83a3f52d1f23c00521a8d4f5942a8
mdulc928 <mdulc928@students.bju.edu>
2020-11-13 21:45:54 -0500

    Updated create-service view

--- create-service.sql
+++ create-service.sql
@@ -1,29 +1,38 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `create_service`(IN service_datetime DATETIME, IN theme VARCHAR(0), in person_id int, in tmpltsvc_id int, out error_msg int)
+CREATE DEFINER=`root`@`localhost` PROCEDURE `create_service`(IN service_datetime DATETIME, IN theme VARCHAR(40), in person_id int, in tmpltsvc_id int, out error_msg int, out newsvc_id int)
 begin
-	declare chk_id int;
+	declare chk_id int;    
     declare svc_id int;
-    select id, max(Service_ID) + 1 into chk_id, svc_id from service 
-    where Svc_DateTime like service_datetime;
+    declare newitem_id int;
+    
+SELECT 
+    Service_ID
+INTO chk_id FROM
+    service
+WHERE
+    Svc_DateTime = service_datetime;
     
     if chk_id is not null then
 		set error_msg = 1;
 	else 
-		INSERT INTO `wso_mysql`.`service` (`Service_ID`, `Svc_DateTime`,`Theme_Event`)
-		VALUES (id, service_datetime, theme);
+		select max(Service_ID) + 1 into svc_id from service;
+        
+		INSERT INTO `wsoapp2`.`service` (`Service_ID`, `Svc_DateTime`,`Theme_Event`)
+		VALUES (svc_id, service_datetime, theme);
         
         if person_id is not null then        
-			insert into `wso_mysql`.`fills_role` values(person_id, svc_id, 'S', 'Y');
+			insert into `wsoapp2`.`fills_role`(`Person_ID`,`Service_ID`,`Role_Type`,`Confirmed`)
+            values(person_id, svc_id, 'S', 'Y');
         end if;
         
         if tmpltsvc_id is not null then
+			select max(Service_Item_ID) into newitem_id from service_item;
+			
 			insert into service_item
-				select max(Service_Item_ID) + 1, Seq_Num, null, null, null, null, null, null, Event_Type_ID, svc_id
+				select @newitem_id + 1, svc_id, Seq_Num, Event_Type_ID, null, null, 'Y', null, null, null
                 from service_item where Service_ID = tmpltsvc_id;                
 		end if;
         
+        set newsvc_id = svc_id;
         set error_msg = 0;
     end if;
-    
-
-	
 end
