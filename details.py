815a577cee166252316524a3a59e78026b38a400
Colten Shipe <34792690+Colipe1@users.noreply.github.com>
2020-11-15 00:22:54 -0500

    Update details page

--- details.py
+++ details.py
@@ -17,6 +17,17 @@ def getDetails(svc_id: str, cursor):
     result = cursor.fetchall()
 
     cursor.execute("""
+    SELECT event_type.Description, service_item.Title, CONCAT(person.First_Name, ' ', person.Last_Name) AS Person, Confirmed, ensemble.Name, song.Title AS Song, Notes
+    FROM service_item LEFT JOIN person ON service_item.Person_ID = person.Person_ID
+    LEFT JOIN ensemble ON ensemble.Ensemble_ID = service_item.Service_Item_ID
+    LEFT JOIN song ON song.Song_ID = service_item.Song_ID
+    LEFT JOIN event_type ON event_type.Event_Type_ID = service_item.Event_Type_ID
+    WHERE Service_ID = %s
+    """, (svc_id,))
+    
+    service_items = cursor.fetchall()
+
+    cursor.execute("""
     SELECT *
     FROM person
     """)
@@ -39,33 +50,46 @@ def getDetails(svc_id: str, cursor):
         <th>Date
         <th>Theme
         <th>Song Leader
-        <th>Organist
-        <th>Pianist
-        <th>Sequence No.
+    </tr>
+    """
+
+    tableRow = "<tr>"
+    for item in result[0][1:4]:
+        tableRow += f"""<td style="border: 2px solid black">{item}"""
+    tableRow += "</tr>"
+    table += tableRow
+    table += "</table>"
+
+    table += """
+    &nbsp;
+    <table style="border: 3px solid black; border-collapse: collapse">
+    <tr>
         <th>Description
-        <th>Song Title
+        <th>Title
+        <th>Person
+        <th>Confirmed
         <th>Name
+        <th>Song
         <th>Notes
     </tr>
     """
 
-    for row in result:
-        (Svc_DateTime, Theme_Event, songleader, organist, pianist, Seq_Num, Description, title, name, notes) = row[1:]
+    for row in service_items:
         tableRow = "<tr>"
         itemCount = 0
-        for item in row[1:]:
-            if itemCount == 7: #song title index
-                tableRow += "<td><select>"
-                if row[8] == None:
+        for item in row:
+            if itemCount == 5: #song title index
+                tableRow += """<td style="border: 2px solid black"><select>"""
+                if row[5] == None:
                     tableRow += "<option value="" selected></option>"
                 else:
                     tableRow += "<option value=""></option>"
                 for song in songs:
-                    if song[2] != None and row[8] != None and song[2] in row[8]:
+                    if song[2] != None and row[5] != None and song[2] in row[5]:
                         tableRow += f"""<option value="{song[0]}" selected>{song[2]}</option>"""
                     tableRow += f"""<option value="{song[0]}">{song[2]}</option>"""
                 tableRow += "</select></td>"
-                
+
             else:
                 tableRow += f"""<td style="border: 2px solid black">{item}"""
             itemCount += 1
@@ -81,12 +105,12 @@ def getDetails(svc_id: str, cursor):
             table, result[0][2], optionStr, svc_id)
 
 HTML_DETAILS = """<html><body>
-        <h2>Service Plan</h2>
+        <h2>Service Details</h2>
         {0}</body></html>
         &nbsp; 
         <h2>Create New Service</h2>
         <form method='get' action='create'>
-          Date and time: <input type='datetime-local' name='Svc_DateTime' value='' required>
+          Date and time: *<input type='datetime-local' name='Svc_DateTime' value='' required>
           &nbsp;
           Theme or Event: <input type='text' name='Theme_Event' value="{1}">
           &nbsp;
